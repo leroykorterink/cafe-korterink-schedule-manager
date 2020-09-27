c88857499081c86250e6caa82b771d4c0ad11f13
Leroy Korterink <leroy@mediamonks.com>
2020-09-27 20:01:17 +0200

    Update login page styling

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Redirect, withRouter } from "react-router-dom";
 import { LoginSuccessPath } from "../../../enum/RoutePaths";
 import { AuthContext } from "../../AuthContext";
+import LoginButton from "../../LoginButton";
+import style from "./Login.module.scss";
 
-const Login = props => {
-  if (props.auth2.isSignedIn.get()) {
+const Login = () => {
+  const { auth2 } = useContext(AuthContext);
+
+  if (auth2.isSignedIn.get()) {
     return <Redirect to={LoginSuccessPath} />;
   }
 
-  return <div>Login page</div>;
+  return (
+    <p className={style.wrapper}>
+      <LoginButton auth2={auth2} /> om het rooster te bekijken.
+    </p>
+  );
 };
 
-export default withRouter(props => (
+export default withRouter((props) => (
   <AuthContext.Consumer>
-    {context => <Login {...props} {...context} />}
+    {(context) => <Login {...props} {...context} />}
   </AuthContext.Consumer>
 ));
